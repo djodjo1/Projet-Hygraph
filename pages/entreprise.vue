79d6a53aff663c8f0abff62386b9177d70775e76
djodjo1 <144000504+djodjo1@users.noreply.github.com>
2024-01-08 05:51:54 +0100

    Add files via upload

--- pages/entreprise.vue
+++ pages/entreprise.vue
@@ -3,7 +3,7 @@
         <section>
             <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                 <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                    <h2 class="font-sans mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                    <h2 class="font-sans mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"> <br>
                         Les parfums. Une expertise inégalée chez nous.
                     </h2>
                     <p class="mb-4">
@@ -12,6 +12,20 @@
                     </p>
                     <p>
                         Chez Parfum Femur, notre mission est de propulser le talent émergent, de célébrer la diversité olfactive, et de construire une communauté passionnée autour de la création parfumée authentique.</p>
+
+                        <p>
+
+                            Chez Parfum Femur, notre passion réside dans une exploration sensorielle infatigable. Chaque fragrance que nous accueillons sur notre plateforme est une invitation à un voyage olfactif inédit. Nous croyons en la puissance des parfums pour transcender les limites conventionnelles, transformant chaque vaporisation en une expérience captivante.
+                        </p>
+
+                        <p>
+
+                            Parfum Femur s'engage fermement à propulser le talent émergent dans le monde de la parfumerie. Notre plateforme est un terrain fertile où les créateurs visionnaires sont encouragés à exprimer leur art sans limites. </p>
+
+                        <p>
+                            Chez Parfum Femur, nous avons construit bien plus qu'une plateforme, nous avons forgé une communauté passionnée de connaisseurs de parfums. Chaque membre de notre communauté partage une fascination commune pour l'art olfactif, créant ainsi un espace où les échanges et les découvertes sont célébrés.
+
+                        </p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 mt-8">
                     <img class="w-full rounded-lg object-cover h-96"
